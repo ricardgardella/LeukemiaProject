6c4f24858f070981f9238bfc222f764003c28eac
Ricard gardella garcia <ricardgardellagarcia@MacBook-Pro-de-Ricard.local>
2018-12-24 18:16:18 +0100

    Ready

--- initial.py
+++ initial.py
@@ -1,3 +1,8 @@
 import numpy as np
 import scipy as sp 
+import pandas as pd
 import csv as csv
+
+train = pd.read_csv("data/data_set_ALL_AML_train.csv",sep=";")
+independent = pd.read_csv("data/data_set_ALL_AML_independent.csv",sep=";")
+
